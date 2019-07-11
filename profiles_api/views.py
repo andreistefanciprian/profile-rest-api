7a836d898148d9f285f69afd00748e4bb16876d7
Stefan Andrei <stefan.andrei@virginmedia.co.uk>
2019-07-11 15:28:00 +0100

    add search filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import permissions
@@ -15,6 +15,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
 
 
 class HelloApiView(APIView):
