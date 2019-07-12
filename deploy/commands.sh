222505a4e4973c443b08dc7ca374ace66b4654e8
Stefan Andrei <stefan.andrei@virginmedia.co.uk>
2019-07-12 16:49:54 +0100

    remove commands

--- deploy/commands.sh
+++
@@ -1,8 +0,0 @@
-
-# download setup.sh script
-curl -sL https://raw.githubusercontent.com/andreistefanciprian/profile-rest-api/master/cloud_aws/setup.sh?token=AGXEMXUHZ565DK4LT3YDYSS5GGPXK | sudo bash -
-
-sudo sh ./deploy/update.sh
-
-# create superuser
-sudo env/bin/python manage.py createsuperuser
