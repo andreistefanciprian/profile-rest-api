d458a6b212f4f961a8b7ef2dd14362d31d49e6f6
Stefan Andrei <stefan.andrei@virginmedia.co.uk>
2019-07-12 11:45:30 +0100

    add cloud deploy scripts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e4r%o+##2+dr1^!r)&mr0jku(=#u#@6v@!x2go7qi@kxy&3$*l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 
 # Configure the custom user model
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
